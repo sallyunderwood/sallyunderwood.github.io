e331e7c3b546f0882c981f0e2516a79fd060f0ac
Sally Underwood <sally.underwood01@gmail.com>
2018-04-22 21:58:01 -0700

    Moving everything else

---
+++ forms/form-to-email.php
@@ -0,0 +1,52 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+  
+  $email_from = "$visitor_email \r\n";
+  
+  /*CORRECT THE $to LINE BELOW + THEN DELETE THIS COMMENT LINE*/
+  $to = "underwo6@seattleu.edu";
+  
+  $headers = "From: $visitor_email \r\n";
+  $headers .= "Reply-To: $visitor_email \r\n";
+  $email_subject = "A Message For You";
+  
+  $email_body = "You have received news from your friend $name.\n".   	  "Here's the message:\n $message";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+  /*CORRECT THE Location: LINE BELOW (MAKING SURE OF RELATIVE LINK!)+ THEN DELETE THIS COMMENT LINE*/
+  header('Location:thank-you.html');
+
+
+function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+                
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+     
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+ 
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+?>
